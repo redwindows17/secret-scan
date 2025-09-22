2f2de648d8b82c0082dd85ce34c24a9ee32b2491
HALISH RICHARD J <121237350+redwindow17@users.noreply.github.com>
2025-09-22 19:00:53 +0530

    Rename main.py to mainrr.py

--- mainrr.py
+++ mainrr.py
@@ -1,3 +1,4 @@
 
 GITHUB_TOKEN=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
 client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/test-sa%40my-sample-project-123456.iam.gserviceaccount.com
+GEMINI_API_KEY = AIzaSyA-1234567890abcdefGEMINIAPIKEYEXAMPLE123456
