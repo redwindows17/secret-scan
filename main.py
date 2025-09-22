ddd2a67f74a3d3832090f4956d881c82311de327
HALISH RICHARD J <121237350+redwindow17@users.noreply.github.com>
2025-09-22 18:59:26 +0530

    Add GITHUB_TOKEN and client_x509_cert_url
    Added GITHUB_TOKEN and client_x509_cert_url to main.py

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 
 GITHUB_TOKEN=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
+client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/test-sa%40my-sample-project-123456.iam.gserviceaccount.com
